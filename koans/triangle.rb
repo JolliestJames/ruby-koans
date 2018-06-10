933679973e23b1104d471d32850ea191c9d811e8
ToBeAmped <martinja@oregonstate.edu>
2018-06-10 10:44:49 -0700

    Make test_illegal_triangles_throw_exceptions pass

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -15,7 +15,14 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
-  if a == b && b == c
+  if  a == 0 || b == 0 || c == 0
+    raise TriangleError, "A triangle may not have any side of length 0"
+  elsif a < 0 || b < 0 || c < 0
+    raise TriangleError, "A triangle may not have a side with negative length"
+  elsif ((a + b) <= c) || ((a + c) <= b) || ((b + c) <= a)
+    raise TriangleError, 
+      "The sum of two sides of a triangle must always form a larger number than any one side"
+  elsif a == b && b == c
     :equilateral
   elsif a == b || b == c || a == c
     :isosceles
@@ -26,4 +33,5 @@ end
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError < StandardError
+
 end
